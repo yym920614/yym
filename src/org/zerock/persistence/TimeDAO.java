3a97664bcbcedbdbd52456143259515075023be5
Administrator <Administrator@YouYoungMin>
2016-10-21 12:20:59 +0900

    add jar

--- src/org/zerock/persistence/TimeDAO.java
+++ src/org/zerock/persistence/TimeDAO.java
@@ -7,33 +7,46 @@ import java.sql.Connection;
 
 public class TimeDAO {
 
-   public String getSysdate() throws Exception {
+	public String getSysdate() throws Exception {
 
-      Connection con = null;
-      PreparedStatement pstmt = null;
-      ResultSet rs = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 
-      try {
-         Class.forName("oracle.jdbc.driver.OracleDriver");
-         con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.51:1521:XE", 
-               "bit88", 
-               "bit88");
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.25:1521:XE", "bit88", "bit88");
 
-         pstmt = con.prepareStatement("select sysdate");
+			pstmt = con.prepareStatement("select sysdate from dual");
 
-         rs = pstmt.executeQuery();
+			rs = pstmt.executeQuery();
 
-         while (rs.next()) {
-            return rs.getString(1);
-         }
+			while (rs.next()) {
+				return rs.getString(1);
+			}
 
-      } catch (Exception e) {
-         e.printStackTrace();
-      } finally {
-         if (rs != null) {try {rs.close();} catch (Exception e) {}};
-         if (pstmt != null) {try {pstmt.close();} catch (Exception e) {}};
-         if (con != null) {try {con.close();} catch (Exception e) {}};
-               }
-      return null;
-   }
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (Exception e) {
+				}
+			};
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (Exception e) {
+				}
+			};
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+				}
+			};
+		}
+		return null;
+	}
 }
